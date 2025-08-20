10e594a609f257144336699950af556eedd75145
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-20 10:24:56 +0300

    created models and made migrations

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=512, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self):
+        return f"{self.content[:30]} - {self.created_at}"
