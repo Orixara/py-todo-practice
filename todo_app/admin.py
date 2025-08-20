a1e15383f1a0f8301be741c3cc3939b3f27d082b
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-20 10:31:16 +0300

    registered models in admin and created superuser

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_app.models import Tag, Task
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "is_done", "created_at", "deadline"]
+    list_filter = ["is_done", "created_at", "tags"]
+    search_fields = ["content"]
+    list_editable = ["is_done"]
+    filter_horizontal = ["tags"]
